8ef086e7d333c86ba7e4160ee3ebfc1493da8abc
ilhamsa1 <muhammad.ilhamsa1@gmail.com>
2022-03-12 10:31:14 +0700

    Add styles

--- packages/phoenix-builder/lib/phoenix-builder.js
+++ packages/phoenix-builder/lib/phoenix-builder.js
@@ -1,7 +1,13 @@
-const rollup = require('rollup');
+#!/usr/bin/env node
+console.log('hugkuguk')
 const path = require('path');
+
+const rollup = require('rollup');
+console.log(rollup)
+
 const resolve = require('@rollup/plugin-node-resolve').default;
 const babel = require('@rollup/plugin-babel').default;
+const postcss = require('rollup-plugin-postcss');
 
 const currentWorkingPath = process.cwd();
 const { main, name } = require(path.join(currentWorkingPath, 'package.json'));
@@ -9,7 +15,7 @@ const { main, name } = require(path.join(currentWorkingPath, 'package.json'));
 const inputPath = path.join(currentWorkingPath, main);
 
 // Little workaround to get package name without scope
-const fileName = name.replace('@cddev/', '');
+const fileName = name.replace('@ilhams_dev/', '');
 
 // see below for details on the options
 const inputOptions = {
@@ -17,6 +23,10 @@ const inputOptions = {
   external: ['react'],
   plugins: [
     resolve(),
+    postcss({
+      // Key configuration
+      modules: true,
+    }),
     babel({
       presets: ['@babel/preset-env', '@babel/preset-react'],
       babelHelpers: 'bundled',
